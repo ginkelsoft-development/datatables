1ba676e863c5c685b82f8a7f6a636a29af2edeff
Wietse van Ginkel <info@ginkelsoft.com>
2025-03-07 06:17:24 +0100

    Actions become row actions Remove label bulk actions Remove label per page select update readme

--- resources/views/vendor/datatables/components/bulk-action.blade.php
+++ resources/views/vendor/datatables/components/bulk-action.blade.php
@@ -1,4 +1,3 @@
-<label class="text-sm text-gray-600">{{ __('datatable::datatables.bulk_actions') }}</label>
 <select wire:model="bulkAction"
         class="border border-gray-300 rounded-lg px-3 py-2 text-sm w-48 focus:ring-2 focus:ring-blue-400 transition">
     <option value="">{{ __('datatable::datatables.select_option') }}</option>
